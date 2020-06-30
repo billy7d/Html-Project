b2fa24c3b3d21cf9e5de5ea03bb7c0f4c462f499
Nguyen Hung <billy7d95@gmail.com>
2020-06-30 10:24:13 +0700

    them bai khai bao width

---
+++ [Thực hành] Khai báo và sử dụng biến/thuc-hanh-khai-bao-bien-va-sdung.js
@@ -0,0 +1,12 @@
+let i = 10;
+let f = 20.5;
+let b = true;
+let s = "Hà Nội";
+
+document.write("let i =" + i);
+document.write("<br/>");
+document.write("let f =" + 20.5);
+document.write("<br/>");
+document.write("let b =" + b);
+document.write("<br/>");
+document.write("let s =" + s);
