77cd97ab07b8ea38e9077e32e7c14e219a9ea9f1
Nguyen Hung <billy7d95@gmail.com>
2020-06-30 22:30:05 +0700

    bai mtinh chi dung 1 ham

---
+++ [Bài tập] Ứng dụng máy tính/1-ham.js
@@ -0,0 +1,9 @@
+function nhannut() {
+    var a1 = document.getElementById("so1").value;
+    var b1 = document.getElementById("so2").value;
+    var a2 = parseFloat(a1);
+    var b2 = parseFloat(b2);
+
+
+
+}
