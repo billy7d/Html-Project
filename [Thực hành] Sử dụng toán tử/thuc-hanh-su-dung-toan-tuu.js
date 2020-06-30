254eb7014403cefd160b8c449177eb3d3963306e
Nguyen Hung <billy7d95@gmail.com>
2020-06-30 10:54:14 +0700

    them bai t.hanh su dung toan tu

---
+++ [Thực hành] Sử dụng toán tử/thuc-hanh-su-dung-toan-tuu.js
@@ -0,0 +1,10 @@
+let inputWidth = prompt("Nhap chieu rong");
+let inputHeight = prompt("nhap chieu dai");
+
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+
+let area = width * height;
+document.write("dien tich hinh chu nhat la: " + area);
+
+
