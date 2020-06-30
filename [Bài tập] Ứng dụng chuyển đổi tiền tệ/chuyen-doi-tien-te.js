6ad8e05a20890a254d3d36ce7f4d47f20e769f3a
Nguyen Hung <billy7d95@gmail.com>
2020-06-30 21:25:28 +0700

    them bai chuyen doi tien te

--- [Bài tập] Ứng dụng chuyển đổi tiền tệ/chuyen-doi-tien-te.js
+++ [Bài tập] Ứng dụng chuyển đổi tiền tệ/chuyen-doi-tien-te.js
@@ -1,9 +1,35 @@
-    function convert(){
 
-    let a = document.getElementById("amount").value;
-    let vndusd = a * 0.0000428734;
-    document.write("Ket qua se la:" + vndusd);
+function chuyendoi() {
+    var tien = document.getElementById("amount").value;
+    var nuoc1 = document.getElementById("country1").value;
+    var nuoc2 = document.getElementById("country2").value;
+    var xong = convert(tien, nuoc1, nuoc2);
+
+    alert(xong);
+}
+
+
+
+
+
+
+function convert(tien,nuoc1,nuoc2){
+      var result = tien;
+    if (nuoc1 == nuoc2){
+
+        return tien;
     }
+    if (nuoc1 == "VND" && nuoc2 == "USD"){
+        result = (tien * 0.0000429611);
+
+    }
+    if (nuoc1 == "USD" && nuoc2 == "VND"){
+        result = (tien * 23296.86);
+    }
+    return result;
+}
+
+
 
 
 
