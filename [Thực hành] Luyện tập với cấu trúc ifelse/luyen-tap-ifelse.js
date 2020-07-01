0624ba2b270fea3ff6d8e24a9307fe65b75f59ff
Nguyen Hung <billy7d95@gmail.com>
2020-07-01 20:57:59 +0700

    thuc hanh cau truc ifelse

---
+++ [Thực hành] Luyện tập với cấu trúc ifelse/luyen-tap-ifelse.js
@@ -0,0 +1,10 @@
+var value = prompt('What is the "official" name of JavaScript?', '');
+
+if (value == "ECMAScript") {
+    alert("Right answer");
+} else {
+    alert("Don't know ECMAScript!?");
+}
+
+
+
