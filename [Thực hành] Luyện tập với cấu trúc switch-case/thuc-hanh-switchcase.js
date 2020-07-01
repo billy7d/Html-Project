f4858436369dbf2895b22b0d6881fb9bb9b01b3c
Nguyen Hung <billy7d95@gmail.com>
2020-07-01 22:58:55 +0700

    thuc hanh switchcase bai 1,2

---
+++ [Thực hành] Luyện tập với cấu trúc switch-case/thuc-hanh-switchcase.js
@@ -0,0 +1,32 @@
+switch (browser) {
+        case "Edge":
+            alert("you've got the edge!");
+        break;
+        case "Chrome":
+        case "Firefox":
+        case "Safari":
+        case "Opera":
+            alert("Okay we support these browsers too");
+            break;
+
+            default:
+            alert("We hope that this page looks ok!");
+
+}
+
+
+let a = prompt('a?', '');
+
+switch (a) {
+    case 0:
+        alert(0);
+        break;
+    case 1:
+        alert(1);
+        break;
+    case 2:
+    case 3:
+        alert('2,3');
+        break;
+}
+
