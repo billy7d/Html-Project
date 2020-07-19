a859ab3c4927b68f90b7b0515fbdfc3dd125a0f6
Nguyen Hung <billy7d95@gmail.com>
2020-07-19 21:52:51 +0700

    snake

--- Snake game/snake-thutuc.js
+++ Snake game/snake-thutuc.js
@@ -3,37 +3,48 @@ let ctx= canvas.getContext("2d");
 let gameOver = false;
 
 let snakeDefaultSize = 3;
-let snakeLarge = box = 23;
+let snakeLarge = 23;
+let gridBox = snakeLarge;
 let snakeBody = [];
 let snakeX =  10;
 let snakeY = 10;
 let dX = box;
 let dY = 0;
 
-function render(){
+function render() {
     snakeX = snakeX + dX;
     snakeY = snakeY + dY;
 
-    if (snakeX < 0){
+    if (snakeX < 0) {
         gameOver = true;
     }
-    if (snakeX >=canvas.width){
+    if (snakeX >= canvas.width) {
         gameOver = true;
     }
-    if (snakeY < 0){
+    if (snakeY < 0) {
         gameOver = true;
     }
-    if (snakeY > canvas.height){
+    if (snakeY > canvas.height) {
         gameOver = true;
     }
-        //set snake body
-    snakeBody.unshift({x: snakeX, y:snakeY});
-    if (snakeBody.length > snakeDefaultSize){
+    //set snake body
+    snakeBody.unshift({x: snakeX, y: snakeY});
+    if (snakeBody.length > snakeDefaultSize) {
         snakeBody.pop();
     }
+    for (i = 0; i < snakeBody.length; i++) {
+        ctx.beginPath();
+        ctx.fillRect(snakeBody[i].x, snakeBody[i].y, gridBox, gridBox);
 
-
-
-
+    }
+}
+function drawSnake() {
+    for (i = 0; i < snakeBody.length; i++) {
+        ctx.beginPath();
+        ctx.fillRect(snakeBody[i].x,snakeBody[i].y,gridBox,gridBox);
+    }
 }
 
+drawSnake();
+
+setInterval(render,1000/8);
